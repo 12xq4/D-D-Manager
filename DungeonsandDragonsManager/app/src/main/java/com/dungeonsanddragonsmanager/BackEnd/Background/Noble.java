b74062c543eb89fa58a4b2eaf7d0cf9910144492
Taylor Anderson <taylor-anderson@hotmail.ca>
2015-11-30 16:13:11 -0500

    Update Noble.java

--- DungeonsandDragonsManager/app/src/main/java/com/dungeonsanddragonsmanager/BackEnd/Background/Noble.java
+++ DungeonsandDragonsManager/app/src/main/java/com/dungeonsanddragonsmanager/BackEnd/Background/Noble.java
@@ -8,6 +8,14 @@ import java.util.ArrayList;
 public class Noble extends Background {
     public Noble(){
         name = "Noble";
+        feature = "Position of Privilege \n Thanks to your noble birth, people are inclined to
+think the best of you. You are welcome in high society,
+and people assume you have the right to be wherever
+you are. The common folk make every effort to
+accommodate you and avoid your displeasure, and other
+people of high birth treat you as a member o f the same
+social sphere. You can secure an audience with a local
+noble if you need to.";
         skillset = new ArrayList<>();
         language = new ArrayList<>();
         equipment= new ArrayList<>();
@@ -18,16 +26,20 @@ public class Noble extends Background {
 
     @Override
     void setSkills() {
-
+        skillset.add(new Skills("Skill Proficiency", "History, Persuasion"));
     }
 
     @Override
     void setEquipments() {
-
+        equipment.add("a set of fine clothes");
+        equipment.add("a signet ring");
+        equipment.add("a scroll of pedigree");
+        equipment.add("25 gold");
     }
 
     @Override
     void setLanguage() {
-
+        language.add("Common");
+        language.add("One of your choice");
     }
 }
