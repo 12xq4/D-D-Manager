9634f5b7ff515906e060630c47ece6a599c4700e
Taylor Anderson <taylor-anderson@hotmail.ca>
2015-11-30 15:47:39 -0500

    Update GuildArtisan.java

--- DungeonsandDragonsManager/app/src/main/java/com/dungeonsanddragonsmanager/BackEnd/Background/GuildArtisan.java
+++ DungeonsandDragonsManager/app/src/main/java/com/dungeonsanddragonsmanager/BackEnd/Background/GuildArtisan.java
@@ -8,6 +8,24 @@ import java.util.ArrayList;
 public class GuildArtisan extends Background {
     public GuildArtisan(){
         name = "GuildArtisan";
+        feature = "Guild Membership \n As an established and respected member of a guild, you
+can rely on certain benefits that membership provides.
+Your fellow guild members will provide you with
+lodging and food if necessary, and pay for your funeral
+if needed. In some cities and towns, a guildhall offers a
+central place to meet other members o f your profession,
+which can be a good place to meet potential patrons,
+allies, or hirelings.
+Guilds often wield tremendous political power. If
+you are accused of a crime, your guild will support you
+if a good case can be made for your innocence or the
+crime is justifiable. You can also gain access to powerful
+political figures through the guild, if you are a member
+in good standing. Such connections might require the
+donation of money or magic items to the guild’s coffers.
+You must pay dues of 5 gp per month to the guild. If
+you miss payments, you must make up back dues to
+remain in the guild’s good graces.";
         skillset = new ArrayList<>();
         language = new ArrayList<>();
         equipment= new ArrayList<>();
@@ -18,16 +36,20 @@ public class GuildArtisan extends Background {
 
     @Override
     void setSkills() {
-
+        skillset.add("Skill Proficiency", "Insight, Persuasion");
     }
 
     @Override
     void setEquipments() {
-
+        equipment.add("a set of artisan's tools (one of your choice)");
+        equipment.add("a letter of introduction from your guild");
+        equipment.add("a set of traveler's clothes");
+        equipment.add("15 gold");
     }
 
     @Override
     void setLanguage() {
-
+        language.add("Common");
+        language.add("one of your choice");
     }
 }
