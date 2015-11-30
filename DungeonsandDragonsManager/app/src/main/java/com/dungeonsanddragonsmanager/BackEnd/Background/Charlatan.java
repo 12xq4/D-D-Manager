84d33c7a3b37fd1803339623b60249b7f86c3208
Taylor Anderson <taylor-anderson@hotmail.ca>
2015-11-30 14:52:52 -0500

    Filled in content

--- DungeonsandDragonsManager/app/src/main/java/com/dungeonsanddragonsmanager/BackEnd/Background/Charlatan.java
+++ DungeonsandDragonsManager/app/src/main/java/com/dungeonsanddragonsmanager/BackEnd/Background/Charlatan.java
@@ -18,16 +18,22 @@ public class Charlatan extends Background{
 
     @Override
     void setSkills() {
-
+        skillset.add(new Skills("Skill Proficiency", "Deception", "Slight of Hand"));
     }
 
     @Override
     void setEquipments() {
-
+        equipment.add("a set of fine clothes");
+        equipment.add("a disguise kit");
+        equipment.add("ten stoppered bottles filled with colored liquid OR 
+        /n a set of weighted dice OR
+        /n a deck of marked cards OR
+        /n a signet ring of an imaginary duke");
+        equipment.add("15 gold");
     }
 
     @Override
     void setLanguage() {
-
+        language.add("Common");
     }
 }
