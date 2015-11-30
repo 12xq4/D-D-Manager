0bb6a1b6c403d09f057c122dff13d1733c6cf23a
Taylor Anderson <taylor-anderson@hotmail.ca>
2015-11-30 15:20:37 -0500

    Update Criminal.java

--- DungeonsandDragonsManager/app/src/main/java/com/dungeonsanddragonsmanager/BackEnd/Background/Criminal.java
+++ DungeonsandDragonsManager/app/src/main/java/com/dungeonsanddragonsmanager/BackEnd/Background/Criminal.java
@@ -8,6 +8,12 @@ import java.util.ArrayList;
 public class Criminal extends Background {
     public Criminal(){
         name = "Criminal";
+        feature = "You have a reliable and trustworthy contact who acts as
+your liaison to a network of other criminals. You know
+how to get messages to and from your contact, even
+over great distances; specifically, you know the local
+messengers, corrupt caravan masters, and seedy sailors
+who can deliver messages for you.";
         skillset = new ArrayList<>();
         language = new ArrayList<>();
         equipment= new ArrayList<>();
@@ -18,16 +24,18 @@ public class Criminal extends Background {
 
     @Override
     void setSkills() {
-
+        skillset.add(new Skills("Skill Proficiency", "Deception, Stealth"));
     }
 
     @Override
     void setEquipments() {
-
+        equipment.add("a crowbar");
+        equipment.add("a set of dark common clothes including a hood");
+        equipment.add("15 gp");
     }
 
     @Override
     void setLanguage() {
-
+        language.add("Common");
     }
 }
