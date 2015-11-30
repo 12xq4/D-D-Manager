67f88c5e4fd62b8ef7ef99fc892f29ecb3b2d299
Taylor Anderson <taylor-anderson@hotmail.ca>
2015-11-30 16:08:32 -0500

    Update Hermit.java

--- DungeonsandDragonsManager/app/src/main/java/com/dungeonsanddragonsmanager/BackEnd/Background/Hermit.java
+++ DungeonsandDragonsManager/app/src/main/java/com/dungeonsanddragonsmanager/BackEnd/Background/Hermit.java
@@ -8,6 +8,13 @@ import java.util.ArrayList;
 public class Hermit extends Background {
     public Hermit(){
         name = "Hermit";
+        feature = "Discovery \n The quiet seclusion of your extended hermitage gave you
+access to a unique and powerful discovery. The exact
+nature of this revelation depends on the nature of your
+seclusion. It might be a great truth about the cosmos,
+the deities, or the forces of nature. It could be a site that no one else
+has ever seen. Work with your DM to determine the details of your
+discovery and its impact on the campaign."
         skillset = new ArrayList<>();
         language = new ArrayList<>();
         equipment= new ArrayList<>();
@@ -18,16 +25,21 @@ public class Hermit extends Background {
 
     @Override
     void setSkills() {
-
+        skillset.add(new Skill("Skill Proficiency", "Medicine, Religion")
     }
 
     @Override
     void setEquipments() {
-
+        equipment.add("a scroll case full of stuffed notes from your studies or prayers");
+        equipment.add("a winter blanket");
+        equipment.add("a set of common clothes");
+        equipment.add("a set of common clothes");
+        equipment.add("15 gold");
     }
 
     @Override
     void setLanguage() {
-
+        language.add("Common");
+        language.add("One of your choice");
     }
 }
